1726ec6c6b3c2d65007b3df6e85c2854354c8ea3
Jatin Chauhan <jatinchauhan615@gmail.com>
2024-07-17 14:40:53 +0000

    promise

--- 08_async_js/promises.js
+++ 08_async_js/promises.js
@@ -1,7 +1,7 @@
 const promiseOne = new Promise(function(resolve, reject){                           // Creation of a promise
     //Do an async task
     // DB calls, cryptography, network
-    setTimeout(function(){
+    setTimeout(function(){                                                          
         console.log('Async task is compelete');
         resolve()                                                                   // when resolve is called, the callback function inside the then() function is executed
     }, 1000)
@@ -50,7 +50,7 @@ const promiseFour = new Promise(function(resolve, reject){
                                                                                     // through this chaining we can keep passing and calling subsequent callback functions(written inside then) without creating a callback hell
 }).then((username) => {
     console.log(username);
-}).catch(function(error){                                                           // if reject() is called inside the promise then the callback function inside catch() is executed
+}).catch(function(error){                                                           // if reject() is called inside the promise or any error occurs in then callbacks, the callback function inside catch() is executed
     console.log(error);
 }).finally(() => console.log("The promise is either resolved or rejected"))         // callback function inside finally() is executed regardless of whether the promise is resolved or rejected
 
@@ -69,10 +69,11 @@ const promiseFive = new Promise(function(resolve, reject){
 
 async function consumePromiseFive(){                                                // besides then and catch promise can be consumed using async...await as well
     try {
-        const response = await promiseFive
+        const response = await promiseFive                                          // await stops the further execution in async function till the promiseFive is resolved or rejected
+                                                                                    // and the runtime continues with other tasks outside the async function  
         console.log(response);
     } catch (error) {                                                               // rejections in promises are handled using catch block
-                                                                                    // if reject() is called inside promise catch block is executed with the argument passed inside reject 
+                                                                                    // if any error occurs while executing asynchronous or synchronous tasks inside try block catch block is executed
         console.log(error);
     }
 }
@@ -81,9 +82,10 @@ consumePromiseFive()
 
 // async function getAllUsers(){
 //     try {
-//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')       // fetch() also returns a promise with the message of whether the promise is fulfilled or not
+                                                                                            // along with the response if the promise is fulfilled
 
-//         const data = await response.json()
+//         const data = await response.json()                                               // response.json also returns a promise and is an asynchronous task therefore we need to use await keyword with it
 //         console.log(data);
 //     } catch (error) {
 //         console.log("E: ", error);
@@ -101,5 +103,8 @@ fetch('https://api.github.com/users/hiteshchoudhary')
 })
 .catch((error) => console.log(error))
 
-// promise.all
-// yes this is also available, kuch reading aap b kro.
+// Promise.all()                                                                            // this static method is a part of promise concurrency methods(methods that perform concurrent execution of promises) 
+                                                                                            // takes an iterable of promises as an input and returns a single promise object
+                                                                                            // the returned promise is fulfilled only if all of the input promises are fulfilled
+                                                                                            // if any of the input promise is rejected, the returned promise is rejected with the first rejection reason
+                                                                                            // if all the promises are fulfilled, the returned promise is fulfilled with an array of fulfillment values
